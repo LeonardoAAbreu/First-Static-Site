701e0d29e23f2a914cf0bd50955883a1c27337de
Leonardo Abreu <leonardoavelinoabreu@gmail.com>
2026-05-13 17:15:19 -0300

    site ready to deploy

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import os
 import shutil
+import sys
+from pathlib import Path
 
 from inline_markdown import markdown_to_html_node
 
 
 def main():
-    destination = "./public"
+    destination = "./docs"
 
     if os.path.exists(destination):
         shutil.rmtree(destination)
@@ -13,7 +15,7 @@ def main():
 
     copy("./static", destination)
 
-    generate_page("./content/index.md", "./template.html", "./public/index.html")
+    generate_pages_recursive("./content", "./template.html", destination)
 
 
 def copy(source, destination):
@@ -56,12 +58,51 @@ def generate_page(from_path, template_path, dest_path):
 
     template = template.replace("{{ Title }}", title_of_page)
     template = template.replace("{{ Content }}", html_string)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
 
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     open_dest = open(dest_path, mode="w")
     open_dest.write(template)
-    # copy(os.path.join(template_path), os.path.join(dest_path))
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    open_dir = os.listdir(dir_path_content)
+
+    for arch in open_dir:
+        if os.path.isfile(os.path.join(dir_path_content, arch)):
+            if arch.endswith(".md"):
+                open_md = open(os.path.join(dir_path_content, arch))
+                contents = open_md.read()
+                html_contents = markdown_to_html_node(contents)
+                html_contents = html_contents.to_html()
+                open_template = open(template_path)
+                template = open_template.read()
+                title_of_page = extract_title(contents)
+
+                template = template.replace("{{ Title }}", title_of_page)
+                template = template.replace("{{ Content }}", html_contents)
+
+                dest_path = Path(os.path.join(dest_dir_path, arch)).with_suffix(".html")
+
+                os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+                open_dest = open(dest_path, mode="w")
+                open_dest.write(template)
+            else:
+                pass
+        else:
+            generate_pages_recursive(
+                os.path.join(dir_path_content, arch),
+                template_path,
+                os.path.join(dest_dir_path, arch),
+            )
 
 
 if __name__ == "__main__":
     main()
+
+
+if len(sys.argv) > 1:
+    basepath = sys.argv[1]
+else:
+    basepath = "/"
